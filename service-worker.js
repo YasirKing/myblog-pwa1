d8acafaa1dabb14e38a23893b90f1843363c80c5
YasirKing <65265870+YasirKing@users.noreply.github.com>
2026-03-08 11:57:00 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "blogger-pwa";
+
+self.addEventListener("install", function(event){
+event.waitUntil(
+caches.open(CACHE_NAME).then(function(cache){
+return cache.addAll(["/"]);
+})
+);
+});
+
+self.addEventListener("fetch", function(event){
+event.respondWith(
+caches.match(event.request).then(function(response){
+return response || fetch(event.request);
+})
+);
+});
